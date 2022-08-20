f6673fa8ab31fb4e72d493e59fe026cf10bbd7da
idehen osariemen <g.o.idehen@gmail.com>
2022-08-20 17:05:47 +0100

    task completed

--- sort.h
+++ sort.h
@@ -1,9 +1,32 @@
 #ifndef SORT_H
 #define SORT_H
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
+#define SORTED 0
+#define UNSORTED 1
+
+/*----------------- MACROS FUNCTIONS ----------------- */
+/**
+ * SWAP - function is used to swap two va riables.
+ * - Parameters
+ * @VARIABLE_A: variable to swap
+ * @VARIABLE_B: variable to swap
+ * @TYPE: type of the variables
+ * - Locals
+ * #TMP_SWAP: temporal variable for to save value
+ */
+#define SWAP(VARIABLE_A, VARIABLE_B, TYPE) \
+	do {                                   \
+		TYPE TMP_SWAP = VARIABLE_A;        \
+		VARIABLE_A = VARIABLE_B;           \
+		VARIABLE_B = TMP_SWAP;             \
+	} while (0)
+
+
+
+
+/* DATA STRUCTURE */
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -13,26 +36,25 @@
  */
 typedef struct listint_s
 {
-	const int n;
+	int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
-void print_list(const listint_t *list);
+/* FUNCTIONS */
+/* Prints an array of integers */
 void print_array(const int *array, size_t size);
-void swap(int *array, int idxa, int idxb, size_t size);
-listint_t *swap_list(listint_t *x, listint_t *y, listint_t **list);
-int partition(int *array, int lb, int ub, size_t size);
-
+/* Prints a list of integers */
+void print_list(const listint_t *list);
+/* Sorts an array of integers in ascending order */
+/* using the Bubble sort algorithm */
 void bubble_sort(int *array, size_t size);
+/* Sorts a doubly linked list of integers in ascending order */
+/*using the Insertion sort algorithm */
 void insertion_sort_list(listint_t **list);
+/* Sorts an array of integers in ascending order */
+/* using the Selection sort algorithm */
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void recursive_sort(int *array, int lb, int ub, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
 
 #endif
